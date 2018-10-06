6068527e4d066ac4b7a85f86e633bcbb7bed6bf9
christones <c.tripodis@intale.com>
2018-10-06 12:45:39 +0300

    Put Dynamic Folders , Add bottom up method vs Single forecasting

--- Forecasting.r
+++ Forecasting.r
@@ -1,6 +1,6 @@
 
 ### Thesis Starts Here
-# test
+
 ### Get Libraries
 install.packages('astsa')
 install.packages('forecast')
@@ -13,15 +13,34 @@ library(data.table)
 library(readxl)
 library(dplyr)
 library(readxl)
+library(ggplot2)
+library(metrics)
+
+##### Parameters Session
+LastWeek    <-30       #Last week of the whole dataset
+LastWeekTest <-5       #Last Week of the Test Dataset
+FOrecastingLength <-5  #Foreacsting length on the Test
+
+#### Create Folder Structure
+now<-Sys.time()
+OutDir <- "C:\\Users\\ctripodis\\Desktop\\AllFiles\\AUEB\\Thesis\\Output"
+ModelDir<-paste0(format(now, "%Y%m%d_%H%M%S"))
+dir.create(file.path(OutDir,(ModelDir)))
+
+ChartDir<-file.path(ModelDir,"Charts")
+dir.create(file.path(OutDir,(ChartDir)))
+
+FilesDir<-file.path(ModelDir,"Files")
+dir.create(file.path(OutDir,(FilesDir)))
 
 ### Import Session
 Sys.setlocale(category = "LC_ALL", locale = "Greek")
 setwd("C:\\Users\\ctripodis\\Desktop\\AllFiles\\AUEB\\Thesis\\Input")
 
-ThesisData <-fread("DataBeers.csv", sep=",",header = T,encoding="UTF-8")
+ThesisData <-fread("databeverages09291.csv", sep=",",header = T,encoding="UTF-8")
 ThesisData<- ThesisData[ ,  .(Allqty=sum(Allqty),
                               Allrevenues=sum(Allrevenues)),
-                     by = .(Cust_Point_UniqueId,Cust_Description,Cust_RPS_Name
+                         by = .(Cust_Point_UniqueId,Cust_Description,Cust_RPS_Name
                             ,Loc_Ael_1,week,year,BrandID	,BrandName,Prod_CatID) ]
 
 Brands<-read.xlsx("Top20Brands.xlsx",sheet ="PickBrand", fill=TRUE )
@@ -44,7 +63,7 @@ ThesisData<-ThesisData[order(Cust_Point_UniqueId,year,week)]
 
 Week1<- data.frame("Year" = 2016,"Week" = ((1:52)))
 Week2<- data.frame("Year" = 2017,"Week" = ((1:52)))
-Week3<- data.frame("Year" = 2018,"Week" = ((1:30)))
+Week3<- data.frame("Year" = 2018,"Week" = ((1:LastWeek)))
 
 Week<-rbind(Week1,Week2,Week3)
 Week<-data.table(Week)
@@ -56,222 +75,206 @@ y<-unique(ThesisData$BrandName)
 n1<-length(x)*length(y)
 n2<- nrow(Week)
 
-Fitted<-data.frame(matrix(nrow=n2,ncol=1))
-Actual<-data.frame(matrix(nrow=n2,ncol=1))
+Fitted<-data.frame()
+ForecastStartd<-data.frame()
 ForecastStart<-numeric()
 
-dev.off() 
+#Set up Goodnes of fit Functions
+R2 <- function(actual,predict){
+  round(1 - (sum((actual-predict )^2)/sum((actual-mean(actual))^2)),2)
+}
+my_RMSE<-function(actual,predict){round(sqrt(mean((predict -actual)^2)),0)}
+
+
+mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
+                    ,paste("Individual_Fit_Charts.pdf"))
+pdf(file=mypath1)
+
 for ( j in x)
   for ( i in y)
     {
       {
         Temp<-ThesisData[Cust_Point_UniqueId==j & BrandName==i,] 
-       # Temp<-ThesisData[Cust_Point_UniqueId=='ip10030' & BrandName=='FIX',] 
+        if(nrow(Temp) ==0){next}
         t<- merge(x=Week, y=Temp, by.x=c("Year","Week"),by.y=c("year","week")   , all.x=TRUE)
-        
         t[is.na(t$Cust_Point_UniqueId),"Cust_Point_UniqueId"]=unique(Temp$Cust_Point_UniqueId)
         t[is.na(t$Cust_Description),"Cust_Description"]=unique(Temp$Cust_Description)
         t[is.na(t$Cust_RPS_Name),"Cust_RPS_Name"]=unique(Temp$Cust_RPS_Name)
         t[is.na(t$Loc_Ael_1),"Loc_Ael_1"]=unique(Temp$Loc_Ael_1)
         t[is.na(t$BrandID),"BrandID"]=unique(Temp$BrandID)
         t[is.na(t$BrandName),"BrandName"]=unique(Temp$BrandName)
-      #  t[is.na(t$Man_Name),"Man_Name"]=unique(Temp$Man_Name)
         t[is.na(t$Prod_CatID),"Prod_CatID"]=unique(Temp$Prod_CatID)
         t[is.na(t$Allqty),"Allqty"]=0
         t[is.na(t$Allrevenues),"Allrevenues"]=0
         
-        
         print(paste("Customer =" ,j, "Brand =" , i))
-        ThesisDataTime<-ts(t[,Allqty],start=c(2016,31),frequency=52)
+        ThesisDataTime<-ts(t[,Allqty],start=c(2016,LastWeek+1),frequency=52)
         Model<-auto.arima(ThesisDataTime)
         summary(Model)
         Fit<-ThesisDataTime-residuals(Model)
         
-        pdf(paste("Fitt Customer =" ,j, "Brand =" , i,".pdf"))
-          ts.plot(ThesisDataTime)
-          points(Fit,type="l",col="red")
-        dev.off()
+        Rsq<-R2(Model$x,Model$fitted)
+        Rmse<-my_RMSE(Model$x,Model$fitted)
         
-          fcast <- forecast(Model, h=20)
-       
-         pdf(paste("Forecast Customer =" ,j, "Brand =" , i,".pdf"))
-          plot(fcast, sub=paste("Customer =" ,j, "Brand =" , i))
-         dev.off()
+        #Output Fit charts
+      
+          ts.plot(ThesisDataTime, main=paste("Fit Chart Customer =" ,j, "Brand =" , i),
+                  sub=paste("Total Rsq=" ,Rsq ,"Rmse = ", Rmse))
+          points(Fit,type="l",col="red")
+
+
          #Strore Fitted and Actual Values
-         Fitted1<-data.frame(Model$fitted)
-         Fitted<-cbind(Fitted,Fitted1)
-         
-         Actual1<-data.frame(Model$x)
-         Actual<-cbind(Actual,Actual1)
+         Fitted1<-data.frame(Actual=as.numeric(Model$x),Fit=as.numeric(Model$fitted),Outlet=j,Brand=i)
+         Fitted1$ID <- seq.int(nrow(Fitted1))
+         Fitted<-rbind(Fitted,Fitted1)
          
          #### Repeat Exercise in Test and Validation Dataset
          
-         Model1<-auto.arima(ts(ThesisDataTime[1:(length(ThesisDataTime)-5)],start=c(2016,31),frequency=52))
-         fcast_no_holdout <- forecast(Model1,h=5)
+         Model1<-auto.arima(ts(ThesisDataTime[1:(length(ThesisDataTime)-LastWeekTest)],start=c(2016,31),frequency=52))
+         fcast_no_holdout <- forecast(Model1,h=FOrecastingLength)
          
-         pdf(paste("Forecast In Test Train Dataset Customer =" ,j, "Brand =" , i,".pdf"))
-           
-          plot(fcast_no_holdout, main=" ")
-          lines(ThesisDataTime)
-          
-          dev.off()
-          
+         plot(fcast_no_holdout, main=paste("Forecasting Customer =" ,j, "Brand =" , i))
+         lines(ThesisDataTime)
+        
           Forecast1<-as.numeric(fcast_no_holdout$mean)
           ForecastStart<-rbind(ForecastStart,Forecast1)
+          
+          ForecastStartd1<-data.frame(Fcast=as.numeric(fcast_no_holdout$mean),
+                                      Outlet=j,Brand=i)
+          
+          ForecastStartd1$ID <- seq.int(nrow(ForecastStartd1))+n2-ForecasthAhaid
+          ForecastStartd<-rbind(ForecastStartd,ForecastStartd1)
          
       }
   } 
-
-
-plot(fcast)
 dev.off()
-write.csv(Fitted,"Fitted.csv",row.names = FALSE)
-write.csv(Actual,"Actual.csv",row.names = FALSE)
-write.csv(ForecastStart,"ForecastStart.csv",row.names = FALSE)
+
+#Merge Fitted With Forecast
+Fitted<-merge(x=Fitted,y=ForecastStartd,by=c("ID","Outlet","Brand") ,all=TRUE)
+Fitted[is.na(Fitted)] <- 0
+
+Fitted<- data.table(Fitted)
+FittedOutlet<- Fitted[ ,  .(Actual=sum(Actual),
+                            Fit=sum(Fit),
+                            Fcast=sum( Fcast)),
+                               by = .(Outlet, ID) ]
+
+FittedBrand<- Fitted[ ,  .(Actual=sum(Actual),
+                            Fit=sum(Fit),
+                           Fcast=sum( Fcast)),
+                       by = .(Brand, ID) ]
 
 
-PmiDataTime<-ts(Temp[,Allqty],start=c(2016,31),frequency=52)
-plot(PmiDataTime)
-acf((PmiDataTime))
-pacf((PmiDataTime))
+FittedTotal<- Fitted[ ,  .(Actual=sum(Actual),
+                           Fit=sum(Fit),
+                           Fcast=sum( Fcast)),
+                         by = .( ID) ]
 
-Model1<-auto.arima(PmiDataTime)
+mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
+                     ,paste("Outlet_Fit_Charts.pdf"))
+pdf(file=mypath1)
 
-Fit<-PmiDataTime-residuals(Model1)
-ts.plot(PmiDataTime)
-points(Fit,type="l",col="red")
-Model1 %>% forecast(h=30) %>% autoplot()
-
-
-############################################################
-# Working Session
-############################################################
-data = read.csv('http://ucanalytics.com/blogs/wp-content/uploads/2015/06/Tractor-Sales.csv')
-data
-dataTime = ts(data[,2],start = c(2003,1),frequency = 12)
-
-births <- scan("http://robjhyndman.com/tsdldata/data/nybirths.dat")
-
-birthstimeseries <- ts(births, frequency=12, start=c(1946,1))
-
-#####################SOM forecast
-#####################
-
-library(RODBC) 
-library(xlsx)
-library(openxlsx)
-library(data.table)
-library(readxl)
-
-SOM         <-read.xlsx("C:\\Users\\ctripodis\\Documents\\PMI\\ToPMI\\HeetsResults091918.xlsx",sheet ="2018", fill=TRUE )
-SOM<- data.table(SOM)
-SOM<-SOM[location=="Athens",c("Market.share","Flag1")]
-SOM[is.na(SOM$Flag1),"Flag1"]=0
-SOM[is.na(SOM$Flag2),"Flag2"]=0
-
-fc<-ses(SOMTime,h=5)
-summary(fc)
-
-SOMTime<-ts(SOM,start=1,end = 30)
-plot(SOMTime)
-plot(diff(SOMTime))
-
-fc1<-holt(SOMTime,h=22)
-fc2<-holt(SOMTime,damped=TRUE,h=7)
-fc1
-
-SomModel<-auto.arima(SOMTime[,"Market.share"],xreg = SOMTime[,"Flag1"])
-SomModel
-ts.plot(SOM)
-points(SomModel$fitted,type="l",col="red")
-fcast <- forecast(SomModel, xreg=rep(1,1) )
-
-plot(fcast)
-fcast
-acf2(diff(SOMTime))
-
-
-##########################
-x<-unique(ThesisData$Cust_Point_UniqueId)
+#Output Total Outlet Fit
 for ( j in x){
-  Temp<-ThesisData[Cust_Point_UniqueId=='ip10038' & BrandID==448,] 
-  
- t<- merge(x=Week, y=Temp, by.x=c("Year","Week"),by.y=c("year","week")   , all.x=TRUE)
- t[is.na(t$Cust_Point_UniqueId),"Cust_Point_UniqueId"]=unique(Temp$Cust_Point_UniqueId)
- t[is.na(t$Cust_Description),"Cust_Description"]=unique(Temp$Cust_Description)
- t[is.na(t$Cust_RPS_Name),"Cust_RPS_Name"]=unique(Temp$Cust_RPS_Name)
- t[is.na(t$Loc_Ael_1),"Loc_Ael_1"]=unique(Temp$Loc_Ael_1)
- t[is.na(t$BrandID),"BrandID"]=unique(Temp$BrandID)
- t[is.na(t$BrandName),"BrandName"]=unique(Temp$BrandName)
- t[is.na(t$Man_Name),"Man_Name"]=unique(Temp$Man_Name)
- t[is.na(t$Prod_CatID),"Prod_CatID"]=unique(Temp$Prod_CatID)
- t[is.na(t$Allqty),"Allqty"]=0
- t[is.na(t$Allrevenues),"Allrevenues"]=0
-
- 
- write.csv(Temp,"t.csv",row.names = FALSE)
- 
   print(j)
-  ThesisDataTime<-ts(Temp[,Allqty],start=c(2016,31),frequency=52)
-  Model<-auto.arima(ThesisDataTime)
-  Model1<-auto.arima(ts(ThesisDataTime[1:(length(ThesisDataTime)-5)],start=c(2016,31),frequency=52))
-  fcast_no_holdout <- forecast(Model1,h=5)
-  #ts.plot(ThesisDataTime[1:(length(ThesisDataTime)-5)])
-  plot(fcast_no_holdout, main=" ")
-  lines(ThesisDataTime)
+  FittedOutletInd<- FittedOutlet[Outlet==j]
   
+  Rsq<-R2(FittedOutletInd$Actual,FittedOutletInd$Fit)
+  Rmse<-my_RMSE(FittedOutletInd$Actual,FittedOutletInd$Fit)
   
-  summary(Model)
-  Fit<-ThesisDataTime-residuals(Model)
-  ts.plot(ThesisDataTime)
-  points(Fit,type="l",col="red")
-  fcast <- forecast(Model, h=20)
-Model$fitted
-    plot(fcast,sub="sub-title" )
-  Model %>% forecast(h=30) %>% autoplot()
+  print(ggplot(FittedOutletInd,aes(x=ID))+geom_line(aes(y=Actual)) +
+  geom_line(aes(y=Fit,colour="Fit"))+  geom_line(aes(y=Fcast,colour="Fcast"))+
+    ggtitle(paste("Outlet = " , j 
+                                                    ,"Rsq = " ,Rsq ,"Rmse = ", Rmse)))
+ 
 }
 
+dev.off() 
+#Output Total Brand Fit
+mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
+                     ,paste("Brand_Fit_Charts.pdf"))
+pdf(file=mypath1)
 
+for ( j in y){
+  print(j)
+  FittedBrandInd<- FittedBrand[Brand==j]
+  
+  Rsq<-R2(FittedBrandInd$Actual,FittedBrandInd$Fit)
+  Rmse<-my_RMSE(FittedBrandInd$Actual,FittedBrandInd$Fit)
+  
+  print(ggplot(FittedBrandInd,aes(x=ID))+geom_line(aes(y=Actual)) +
+          geom_line(aes(y=Fit,colour="Fit"))+geom_line(aes(y=Fcast,colour="Fcast"))+
+          ggtitle(paste("Brand = " , j 
+                                                            ,"Rsq = " ,Rsq ,"Rmse = ", Rmse)))
+  
+}
 
+dev.off() 
+#Output Total Fit
 
-PmiDataTime<-ts(Temp[,Allqty],start=c(2016,31),frequency=52)
-plot(PmiDataTime)
-plot(diff(PmiDataTime))
-acf((PmiDataTime))
-pacf((PmiDataTime))
-acf2(PmiDataTime,max.lag=52)
+mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
+                     ,paste("Grant Total_Fit_Charts.pdf"))
+pdf(file=mypath1)
 
-acf(diff(PmiDataTime),max.lag=52)
-pacf(diff(PmiDataTime),max.lag=52)
+  Rsq<-R2(FittedTotal$Actual,FittedTotal$Fit)
+  Rmse<-my_RMSE(FittedTotal$Actual,FittedTotal$Fit)
+  
+  ggplot(FittedTotal,aes(x=ID))+geom_line(aes(y=Actual)) +
+  geom_line(aes(y=Fit,colour="Fit"))+ geom_line(aes(y=Fcast,colour="Fcast"))+
+    ggtitle(paste("Grant Total Rsq=" ,Rsq ,"Rmse = ", Rmse))
 
-Model1<-auto.arima(PmiDataTime)
+dev.off()
 
-Fit<-PmiDataTime-residuals(Model1)
-ts.plot(PmiDataTime)
+#### Run Forecasting At Grant total
+mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
+                     ,paste("Grant Total_Fit_Charts_Direct.pdf"))
+pdf(file=mypath1)
+
+FittedTotalTime<-ts(FittedTotal[,Actual],start=c(2016,LastWeek+1),frequency=52)
+Model<-auto.arima(FittedTotalTime)
+summary(FittedTotalTime)
+Fit<-FittedTotalTime-residuals(Model)
+
+Rsq<-R2(Model$x,Model$fitted)
+Rmse<-my_RMSE(Model$x,Model$fitted)
+
+ts.plot(FittedTotalTime, main=paste("GrantTotal"),
+        sub=paste("Total Rsq=" ,Rsq ,"Rmse = ", Rmse))
 points(Fit,type="l",col="red")
-Model1 %>% forecast(h=30) %>% autoplot()
 
-i1<-which(x==x[i])
+Model1<-auto.arima(ts(FittedTotalTime[1:(length(FittedTotalTime)-LastWeekTest)],start=c(2016,31),frequency=52))
+fcast_no_holdout <- forecast(Model1,h=FOrecastingLength)
 
-Actual[,i]<-Model$fitted
+plot(fcast_no_holdout, main=paste("Forecasting GrantTotal"))
+lines(FittedTotalTime)
 
-i1<-which(x==x[i])
-Actual[,i1]<-Model$fitted
+dev.off()
 
-############
-SOMData <-fread("C:\\Users\\ctripodis\\Documents\\PMI\\DataPulls\\SOMForecasting\\SOM.csv", sep=",",header = T,encoding="UTF-8")
-SOM<- data.table(SOMData)
-SOM<-SOM[location=="Other","Marketshareincecomm"]
-SOM$Marketshareincecomm<-as.numeric(SOM$Marketshareincecomm)
-SOMTime<-ts(SOM,start=1,end = 9)
 
-Model1<-auto.arima(SOMTime)
-Fit<-SOMTime-residuals(Model1)
-ts.plot(SOMTime)
-points(Fit,type="l",col="red")
-fcast <- forecast(Model1, h=3)
-plot(fcast)
-fcast$fitted
-fcast
+
+
+### Compare Grant Total vs Bottom up (Do we need to do the same for Brand/Outlet Totals/)
+
+
+ExportPath<-file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Files","Fitted.csv")
+write.csv(Fitted, ExportPath ,row.names = FALSE)
+
+ExportPath<-file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Files","ForecastStartd.csv")
+write.csv(ForecastStartd, ExportPath ,row.names = FALSE)
+
+
+
+
+#Calculate totals Level 1 Leve2
+
+
+
+
+
+
+
+
+
+
 
 
