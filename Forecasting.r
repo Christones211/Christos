b072453619318eb7c435e6449b274d776c3e1ca9
christones <c.tripodis@intale.com>
2018-10-14 12:00:03 +0300

    1.Dynamic Metric added 2.10 length forecast 3.Output ARIMA Model Parameters

--- Forecasting.r
+++ Forecasting.r
@@ -2,8 +2,8 @@
 ### Thesis Starts Here
 
 ### Get Libraries
-install.packages('astsa')
-install.packages('forecast')
+#install.packages('astsa')
+#install.packages('forecast')
 
 library(astsa)
 library(forecast)
@@ -17,9 +17,11 @@ library(ggplot2)
 library(metrics)
 
 ##### Parameters Session
+FirstWeek <-10         #Data starts from week 25 2016
 LastWeek    <-30       #Last week of the whole dataset
-LastWeekTest <-5       #Last Week of the Test Dataset
-FOrecastingLength <-5  #Foreacsting length on the Test
+LastWeekTest <-10       #Last Week of the Test Dataset
+FOrecastingLength <-LastWeekTest  #Foreacsting length on the Test
+Metric <-"Allqty"   #switch between  Allrevenues and Allqty
 
 #### Create Folder Structure
 now<-Sys.time()
@@ -39,7 +41,7 @@ setwd("C:\\Users\\ctripodis\\Desktop\\AllFiles\\AUEB\\Thesis\\Input")
 
 ThesisData <-fread("databeverages09291.csv", sep=",",header = T,encoding="UTF-8")
 ThesisData<- ThesisData[ ,  .(Allqty=sum(Allqty),
-                              Allrevenues=sum(Allrevenues)),
+                              Metric=sum(get(Metric))),
                          by = .(Cust_Point_UniqueId,Cust_Description,Cust_RPS_Name
                             ,Loc_Ael_1,week,year,BrandID	,BrandName,Prod_CatID) ]
 
@@ -61,7 +63,7 @@ ThesisData<-ThesisData[order(Cust_Point_UniqueId,year,week)]
 
 # Create Cartesian Week for Weeks with Zero selling
 
-Week1<- data.frame("Year" = 2016,"Week" = ((1:52)))
+Week1<- data.frame("Year" = 2016,"Week" = ((FirstWeek:52)))
 Week2<- data.frame("Year" = 2017,"Week" = ((1:52)))
 Week3<- data.frame("Year" = 2018,"Week" = ((1:LastWeek)))
 
@@ -78,7 +80,7 @@ n2<- nrow(Week)
 Fitted<-data.frame()
 ForecastStartd<-data.frame()
 ForecastStart<-numeric()
-
+ParametersModel <-numeric()
 #Set up Goodnes of fit Functions
 R2 <- function(actual,predict){
   round(1 - (sum((actual-predict )^2)/sum((actual-mean(actual))^2)),2)
@@ -94,7 +96,7 @@ for ( j in x)
   for ( i in y)
     {
       {
-        Temp<-ThesisData[Cust_Point_UniqueId==j & BrandName==i,] 
+        Temp<-ThesisData[Cust_Point_UniqueId==j & BrandName==i & (week > FirstWeek-1 | year>2016),] 
         if(nrow(Temp) ==0){next}
         t<- merge(x=Week, y=Temp, by.x=c("Year","Week"),by.y=c("year","week")   , all.x=TRUE)
         t[is.na(t$Cust_Point_UniqueId),"Cust_Point_UniqueId"]=unique(Temp$Cust_Point_UniqueId)
@@ -104,11 +106,11 @@ for ( j in x)
         t[is.na(t$BrandID),"BrandID"]=unique(Temp$BrandID)
         t[is.na(t$BrandName),"BrandName"]=unique(Temp$BrandName)
         t[is.na(t$Prod_CatID),"Prod_CatID"]=unique(Temp$Prod_CatID)
-        t[is.na(t$Allqty),"Allqty"]=0
-        t[is.na(t$Allrevenues),"Allrevenues"]=0
+        #t[is.na(t$Allqty),"Allqty"]=0
+        t[is.na(t$Metric),Metric]=0
         
         print(paste("Customer =" ,j, "Brand =" , i))
-        ThesisDataTime<-ts(t[,Allqty],start=c(2016,LastWeek+1),frequency=52)
+        ThesisDataTime<-ts(t[,Metric],start=c(2016,FirstWeek),frequency=52)
         Model<-auto.arima(ThesisDataTime)
         summary(Model)
         Fit<-ThesisDataTime-residuals(Model)
@@ -122,7 +124,6 @@ for ( j in x)
                   sub=paste("Total Rsq=" ,Rsq ,"Rmse = ", Rmse))
           points(Fit,type="l",col="red")
 
-
          #Strore Fitted and Actual Values
          Fitted1<-data.frame(Actual=as.numeric(Model$x),Fit=as.numeric(Model$fitted),Outlet=j,Brand=i)
          Fitted1$ID <- seq.int(nrow(Fitted1))
@@ -130,8 +131,13 @@ for ( j in x)
          
          #### Repeat Exercise in Test and Validation Dataset
          
-         Model1<-auto.arima(ts(ThesisDataTime[1:(length(ThesisDataTime)-LastWeekTest)],start=c(2016,31),frequency=52))
+         Model1<-auto.arima(ts(ThesisDataTime[1:(length(ThesisDataTime)-LastWeekTest)],start=c(2016,FirstWeek),frequency=52))
          fcast_no_holdout <- forecast(Model1,h=FOrecastingLength)
+         
+         ParametersModel1<-arimaorder(Model1)
+         ParametersModel<-rbind(ParametersModel,ParametersModel1)
+         
+         ForecastStartd<-rbind(ForecastStartd,ForecastStartd1)
          
          plot(fcast_no_holdout, main=paste("Forecasting Customer =" ,j, "Brand =" , i))
          lines(ThesisDataTime)
@@ -142,7 +148,7 @@ for ( j in x)
           ForecastStartd1<-data.frame(Fcast=as.numeric(fcast_no_holdout$mean),
                                       Outlet=j,Brand=i)
           
-          ForecastStartd1$ID <- seq.int(nrow(ForecastStartd1))+n2-ForecasthAhaid
+          ForecastStartd1$ID <- seq.int(nrow(ForecastStartd1))+n2-FOrecastingLength
           ForecastStartd<-rbind(ForecastStartd,ForecastStartd1)
          
       }
@@ -169,6 +175,8 @@ FittedTotal<- Fitted[ ,  .(Actual=sum(Actual),
                            Fit=sum(Fit),
                            Fcast=sum( Fcast)),
                          by = .( ID) ]
+
+
 
 mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Charts"
                      ,paste("Outlet_Fit_Charts.pdf"))
@@ -230,7 +238,7 @@ mypath1 <- file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesi
                      ,paste("Grant Total_Fit_Charts_Direct.pdf"))
 pdf(file=mypath1)
 
-FittedTotalTime<-ts(FittedTotal[,Actual],start=c(2016,LastWeek+1),frequency=52)
+FittedTotalTime<-ts(FittedTotal[,Actual],start=c(2016,FirstWeek),frequency=52)
 Model<-auto.arima(FittedTotalTime)
 summary(FittedTotalTime)
 Fit<-FittedTotalTime-residuals(Model)
@@ -242,7 +250,7 @@ ts.plot(FittedTotalTime, main=paste("GrantTotal"),
         sub=paste("Total Rsq=" ,Rsq ,"Rmse = ", Rmse))
 points(Fit,type="l",col="red")
 
-Model1<-auto.arima(ts(FittedTotalTime[1:(length(FittedTotalTime)-LastWeekTest)],start=c(2016,31),frequency=52))
+Model1<-auto.arima(ts(FittedTotalTime[1:(length(FittedTotalTime)-LastWeekTest)],start=c(2016,FirstWeek),frequency=52))
 fcast_no_holdout <- forecast(Model1,h=FOrecastingLength)
 
 plot(fcast_no_holdout, main=paste("Forecasting GrantTotal"))
@@ -250,11 +258,7 @@ lines(FittedTotalTime)
 
 dev.off()
 
-
-
-
 ### Compare Grant Total vs Bottom up (Do we need to do the same for Brand/Outlet Totals/)
-
 
 ExportPath<-file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Files","Fitted.csv")
 write.csv(Fitted, ExportPath ,row.names = FALSE)
@@ -262,12 +266,8 @@ write.csv(Fitted, ExportPath ,row.names = FALSE)
 ExportPath<-file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Files","ForecastStartd.csv")
 write.csv(ForecastStartd, ExportPath ,row.names = FALSE)
 
-
-
-
-#Calculate totals Level 1 Leve2
-
-
+ExportPath<-file.path("C:","Users","ctripodis","Desktop","AllFiles","AUEB","Thesis","Output",ModelDir,"Files","ParametersModel.csv")
+write.csv(ParametersModel, ExportPath ,row.names = FALSE)
 
 
 
@@ -275,6 +275,30 @@ write.csv(ForecastStartd, ExportPath ,row.names = FALSE)
 
 
 
+#work
 
 
 
+Temp<-ThesisData[Cust_Point_UniqueId=="ip10028" & BrandName=="COCA COLA"& (week > FirstWeek-1 | year>2016),] 
+
+if(nrow(Temp) ==0){next}
+t<- merge(x=Week, y=Temp, by.x=c("Year","Week"),by.y=c("year","week")   , all.x=TRUE)
+t[is.na(t$Cust_Point_UniqueId),"Cust_Point_UniqueId"]=unique(Temp$Cust_Point_UniqueId)
+t[is.na(t$Cust_Description),"Cust_Description"]=unique(Temp$Cust_Description)
+t[is.na(t$Cust_RPS_Name),"Cust_RPS_Name"]=unique(Temp$Cust_RPS_Name)
+t[is.na(t$Loc_Ael_1),"Loc_Ael_1"]=unique(Temp$Loc_Ael_1)
+t[is.na(t$BrandID),"BrandID"]=unique(Temp$BrandID)
+t[is.na(t$BrandName),"BrandName"]=unique(Temp$BrandName)
+t[is.na(t$Prod_CatID),"Prod_CatID"]=unique(Temp$Prod_CatID)
+t[is.na(t$Allqty),"Allqty"]=0
+t[is.na(t$Allrevenues),"Allrevenues"]=0
+
+
+ThesisDataTime<-ts(t[,Allqty],start=c(2016,31),frequency=52)
+Model<-auto.arima(ThesisDataTime)
+summary(Model)
+Fit<-ThesisDataTime-residuals(Model)
+
+
+
+print(data.frame(Temp))
